b7c563dec205cda3570ca9cb67ce2e7e96135856
bocheng24 <jeffcgp@hotmail.com>
2021-11-14 23:54:50 +0800

    Init Leaflet map, implement the change center

--- src/Map.js
+++ src/Map.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { MapContainer, TileLayer, useMap } from 'react-leaflet';
+import './Map.css';
+
+function ChangeMapView({ coords }) {
+  const map = useMap();
+  map.setView(coords, map.getZoom());
+
+  return null;
+}
 
 
-function Map() {
+function Map({ center, zoom }) {
+    console.log(center, zoom);
+
     return (
         <div className="map">
-          
+          <MapContainer center={ center } zoom={ zoom }>
+              <TileLayer 
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              />
+              <ChangeMapView coords={ center } />
+          </MapContainer>
         </div>
     )
 }
